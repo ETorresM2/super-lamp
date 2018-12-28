fd726338e220761167ef6977cd159ce3289b6818
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-12-28 02:44:33 -0500

    register endpoint added

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ server.use(express.json());
 server.use(cors());
 server.use(morgan("dev"));
 server.use(helmet());
+
+//============================================================================== Server Check <-----
+server.get("/", (req, res) => {
+  res.json({ api: "running" });
+});
+//============================================================================== Get All Users <-----
+server.get("/users", (req, res) => {
+  db("users").then(users => {
+    res.status(200).json(users);
+  });
+});
+//============================================================================== Register User <-----
+server.post("/register", (req, res) => {
+  db("users")
+    .insert(req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err });
+      console.error(err);
+    });
+});
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => console.log(`\n=== Running on port ${port} ===\n`));
